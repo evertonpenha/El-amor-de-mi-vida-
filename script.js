42eb8c9331c9c96b14d1bc6f1a48dbf3a20357dd
evertonpenha <penhacespedeseverton@gmail.com>
2026-05-30 03:13:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
-const inicio = new Date(‘2026-05-02T00:00:00’);
+const inicio = new Date('2026-05-02T00:00:00');
 
 function actualizar() {
+  const ahora = new Date();
+  const diferencia = ahora - inicio;
 
-const ahora = new Date();
+  const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+  const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+  const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
 
-const diferencia = ahora - inicio;
-
-const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
-
-const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
-
-const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
-
-document.getElementById('contador').innerHTML =
-dias + " días ❤️<br>" +
-horas + " horas ❤️<br>" +
-minutos + " minutos ❤️<br>" +
-segundos + " segundos ❤️";
+  document.getElementById('contador').innerHTML =
+    dias + " días ❤️<br>" +
+    horas + " horas ❤️<br>" +
+    minutos + " minutos ❤️<br>" +
+    segundos + " segundos ❤️";
 }
 
 actualizar();
-
-setInterval(actualizar,1000);
+setInterval(actualizar, 1000);
